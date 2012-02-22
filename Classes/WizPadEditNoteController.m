070ef5d4f937724950158e9d4579cc28ff94f576
wiz <wiz@localhost.(none)>
2012-02-22 16:09:56 +0800

    Signed-off-by: wiz <wiz@localhost.(none)>

--- Classes/WizPadEditNoteController.m
+++ Classes/WizPadEditNoteController.m
@@ -595,7 +595,7 @@
     [titleView addSubview:selectPhoto];
     
     UIButton* attachmentsCheck = [UIButton buttonWithType:UIButtonTypeCustom];
-    [attachmentsCheck setImage:[UIImage imageNamed:@"ipadNewAttach"] forState:UIControlStateNormal];
+    [attachmentsCheck setImage:[UIImage imageNamed:@"newNoteAttach"] forState:UIControlStateNormal];
     [titleView addSubview:attachmentsCheck];
     UIBadgeView* attachmentsCount = [[UIBadgeView alloc] initWithFrame:CGRectMake(25, -5, 20, 20)];
     attachmentsCount.badgeString = [NSString stringWithFormat:@"%d",[self.attachmentSourcePath count]];
