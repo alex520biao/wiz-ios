18d99535393ae928450364991cd61290f4f8838b
wiz <wiz@localhost.(none)>
2012-02-27 15:53:43 +0800

    删除很多无用的类

---
+++ Classes/Globals/WizUiShare/attachmentView/WizCheckAttachments.h
@@ -0,0 +1,13 @@
+//
+//  WizCheckAttachments.h
+//  Wiz
+//
+//  Created by wiz on 12-2-27.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WizCheckAttachments : UIViewController
+
+@end
